b546ff2745f5bad5e07976a14ac079a14a6f62fd
Yusuke Hashimoto <yusuke84moto@gmail.com>
2020-08-31 17:16:02 +0900

    getPieceAtのテストコードを追加

--- chess/testChessBoard.py
+++ chess/testChessBoard.py
@@ -15,7 +15,7 @@ class TestChessBoard(unittest.TestCase):
     def test_addPiece(self):
         for x in range(1,9):
             for y in range(1,9):
-                self.sub_test_addPiece1(x, y, x, y)
+                self.sub_test_addPiece1(x, y, x-1, y-1)
                 pass
         self.sub_test_adddPiece2('a1', 0, 0)
         self.sub_test_adddPiece2('h8', 7, 7)
@@ -24,15 +24,33 @@ class TestChessBoard(unittest.TestCase):
     def sub_test_addPiece1(self, x, y, exp_x, exp_y):
         king = King(Color.WHITE, self.board)
         self.board.addPiece(king, x, y)
-        self.assertEqual(self.board.board[x-1][y-1], king)
+        self.assertEqual(self.board.board[exp_x][exp_y], king)
         self.board.empty()
 
     def sub_test_adddPiece2(self, pos, exp_x, exp_y):
         king = King(Color.WHITE, self.board)
         self.board.addPiece(king, pos)
-#        x =  self.board.posMap[pos[0]]
-#        y = int(pos[1]) - 1
         self.assertEqual(self.board.board[exp_x][exp_y], king)
+        self.board.empty()
+
+    def test_getPieceAt(self):
+        pass
+        self.sub_test_getPieceAt(1, 1)
+        self.sub_test_getPieceAt(8, 8)
+        self.sub_test_getPieceAt2('a1')
+
+    def sub_test_getPieceAt(self, x, y):
+        king = King(Color.BLACK, self.board)
+        self.board.addPiece(king, x, y)
+        result = self.board.getPieceAt(x, y)
+        self.assertEqual(king, result)
+        self.board.empty()
+
+    def sub_test_getPieceAt2(self, pos):
+        king = King(Color.BLACK, self.board)
+        self.board.addPiece(king, pos)
+        result = self.board.getPieceAt(pos)
+        self.assertEqual(king, result)
         self.board.empty()
 
 if __name__ == "__main__":
